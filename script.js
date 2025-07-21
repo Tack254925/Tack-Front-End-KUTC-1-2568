1a60d7afb49842f1ddad355e09723c8845c8d57b
Tack2525459 <Siriwat.boonbanlu@gmail.com>
2025-07-21 17:27:03 +0700

    week11

---
+++ script.js
@@ -0,0 +1,91 @@
+// document.write("Hello Worjd")
+
+// alert("Hejjo Worjd")
+// alert("สวัสดีคับ")
+
+// console.log("ทดสอบระบบสำเร็จ")
+
+// var Fullname ="Siriwat"
+
+// const number = 20; 
+
+// document. write(Fullname);
+// document. write(number);
+
+// let result = "123abc";
+// let total = parseInt (result);
+
+// document. write (total);
+
+
+// let Num = 123;
+// let result = Num.toString();
+// document. write (result);
+
+// let total = typeof(resul);
+// document.write(total);
+
+// let num = 12312121;
+// let Text = typeof (num);
+
+
+// document.write (Text);
+
+// document.write (typeof(num));
+
+// let scoll = 105;
+// if( scoll >= 100){
+//     document.writeln("สอบผ่าน");
+// }
+// else{
+//     document.writeln("สอบตก");
+// }
+
+// let Weather = 9;
+//     if(Weather >= 30){
+//         document.writeln("ร้อนมากครับ");
+//     } else if(Weather >= 10){
+//         document.writeln("เริ่มหนาว");
+//     } else {
+//         document.writeln("น้ำเย็นแล้วจ้า")
+//     }
+
+
+// var Day = 7;
+
+// switch(Day){
+//     case  1 : 
+//         document.writeln("วันจันทร์")
+//         break;
+//            case  2 : 
+//         document.writeln("อังคาร")
+//         break;
+//            case  3 : 
+//         document.writeln("พุธ")
+//         break;
+//            case  4 : 
+//         document.writeln("พฤหัสบดี")
+//         break;
+//            case  5 : 
+//         document.writeln("ศุกร์")
+//         break;
+//            case  6 : 
+//         document.writeln("เสาร์")
+//         break;
+//            case  7 : 
+//         document.writeln("อาทิตย์")
+//         break;
+// }         
+
+
+     //แสดงคำว่า อยากกลับบ้าน
+     //จำนวน 13ครั้ง โดยมีตัวเลขแสดงด้วย
+     //แสดง 1เป็นลำดับแรก
+
+
+    //  let i = 13;
+    // for (i = 1 ; i <=13 ; i = i+1) {
+    //     document.writeln("อยากกลับบ้าน");
+    //     document.writeln(i);
+    // }
+
